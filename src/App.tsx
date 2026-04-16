e88049ab7dec33c5f1951e338d24eb081dbfab57
Edson Romero <eromero@tiprotec.com>
2026-04-16 15:09:26 -0600

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
     <>
-      <div className='bg-cyan-900 w-3xl h-3xl text-white'>
-        hello tailwind
+      <div className='bg-cyan-900 w-full h-3xl text-white'>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+          <Button>Click me</Button>
+        </div>
       </div>
     </>
   )
